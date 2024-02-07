9cb107901594e2fd7275ee079670af47f4847ecd
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-07 19:40:29 +0530

    updated

--- util/Alert.js
+++ util/Alert.js
@@ -17,3 +17,36 @@ function unSucsessUpdateAlert(vale) {
         timer: 1500
     })
 }
+
+function yesNoAlertDelete(value) {
+    Swal.fire({
+        title: 'Do you want to Delete the \n' + value + ' ?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Delete',
+        denyButtonText: `Don't Delete`,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            if (deleteCustomer(value)) {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Delete Successfully',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                $(this).remove();
+            } else {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'error',
+                    title: 'Delete Unsuccessfully',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
+        } else if (result.isDenied) {
+            Swal.fire(value + ' Delete Canceled!', '', 'info')
+        }
+    });
+}
