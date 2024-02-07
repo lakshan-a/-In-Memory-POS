48f4a0bb2395aaa48ddd3caed38693b2f336a9b3
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-07 19:41:36 +0530

    updated

--- util/Alert.js
+++ util/Alert.js
@@ -83,4 +83,27 @@ function yesNoAlertIDelete(value) {
         }
     });
 }
-
+function emptyMassage() {
+    let timerInterval
+    Swal.fire({
+        title: 'Empty Result!',
+        html: 'I will close in <b></b> milliseconds.',
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            timerInterval = setInterval(() => {
+                b.textContent = Swal.getTimerLeft()
+            }, 100)
+        },
+        willClose: () => {
+            clearInterval(timerInterval)
+        }
+    }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log('I was closed by the timer')
+        }
+    })
+}
