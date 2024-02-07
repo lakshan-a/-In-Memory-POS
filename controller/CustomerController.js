24e915864c3361a5038aed20e69abb723b06ba67
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-07 12:42:45 +0530

    updated

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -200,3 +200,79 @@ $("#btnSearchCus").click(function () {
      loadAllCustomersForOption();
  }
 
+ //----------------------------- Table Listener Click & Load To TextFields--------------------------//
+
+ function blindClickEvents() {
+     $("#customerTable>tr").click(function () {
+         let id = $(this).children().eq(0).text();
+         let name = $(this).children().eq(1).text();
+         let address = $(this).children().eq(2).text();
+         let salary = $(this).children().eq(3).text();
+         console.log(id, name, address, salary);
+
+         $("#searchCustomerId").val(id);
+         $("#nameUpdate").val(name);
+         $("#addressUpdate").val(address);
+         $("#salaryUpdate").val(salary);
+
+         $("#searchCIdDelete").val(id);
+         $("#disabledNameDelete").val(name);
+         $("#disabledAddressDelete").val(address);
+         $("#disabledSalaryDelete").val(salary);
+     });
+ }
+
+ //----------------------------Table Listener Double Click & Remove-------------------------------------//
+
+ function dblRowClickEventsCus() {
+     $("#customerTable>tr").on('dblclick', function () {
+         let deleteCusID = $(this).children().eq(0).text();
+         yesNoAlertDelete(deleteCusID);
+     });
+ }
+
+ /**
+  * Generate New Customer ID
+  * */
+ function generateCustomerID() {
+     if (customers.length > 0) {
+         let lastId = customers[customers.length - 1].id;
+         let digit = lastId.substring(6);
+         let number = parseInt(digit) + 1;
+         return lastId.replace(digit, number);
+     } else {
+         return "C00-001";
+     }
+ }
+
+ //----------------------------Input Fields warnings in +New Customer------------------------------------//
+
+ $("#txtCustomerId").focus();
+ const regExCusID = /^(C00-)[0-9]{3,4}$/;
+ const regExCusName = /^[A-z ]{3,20}$/;
+ const regExCusAddress = /^[A-z0-9/ ]{4,30}$/;
+ const regExSalary = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
+ let customerValidations = [];
+ customerValidations.push({
+     reg: regExCusID,
+     field: $('#txtCustomerId'),
+     error: 'Customer ID Pattern is Wrong : C00-001'
+ });
+ customerValidations.push({
+     reg: regExCusName,
+     field: $('#txtCustomerName'),
+     error: 'Customer Name Pattern is Wrong : A-z 3-20'
+ });
+ customerValidations.push({
+     reg: regExCusAddress,
+     field: $('#txtCustomerAddress'),
+     error: 'Customer Address Pattern is Wrong : A-z 0-9 ,/'
+ });
+ customerValidations.push({
+     reg: regExSalary,
+     field: $('#txtCustomerSalary'),
+     error: 'Customer Salary Pattern is Wrong : 100 or 100.00'
+ });
+
+
