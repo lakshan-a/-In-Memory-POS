3e2dce77a03e0ce90b6359ae2f6e72ae9b704321
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-11 20:32:37 +0530

    updated

---
+++ assets/styles/bootstrap-5.2.1/js/header.js
@@ -0,0 +1,12 @@
+let nav_home = document.getElementById("nav-home");
+let nav_customer = document.getElementById("nav-customer");
+let nav_order = document.getElementById("nav-order");
+let nav_item = document.getElementById("nav-item");
+let nav_orderD = document.getElementById("nav-order-detail");
+let main_root = document.getElementById("main-root");
+
+let nav_customer_txt = document.getElementById("nav-customer-txt");
+let nav_item_txt = document.getElementById("nav-item-txt");
+let nav_order_txt = document.getElementById("nav-order-txt");
+let nav_home_txt = document.getElementById("nav-home-txt");
+let nav_order_detail_txt = document.getElementById("nav-orderD-txt");
