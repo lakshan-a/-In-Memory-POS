7e654e0b2848ae678326f6b68e9b1ead11f9ff9b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-11 20:33:40 +0530

    updated

--- assets/styles/bootstrap-5.2.1/js/header.js
+++ assets/styles/bootstrap-5.2.1/js/header.js
@@ -33,3 +33,26 @@ function startTime() {
 }
 
 startTime();
+
+nav_customer.addEventListener("click", function () {
+
+    nav_customer_txt.style.fontSize = `21px`;
+    nav_customer_txt.style.color = `red`;
+
+    nav_item_txt.style.fontSize = `20px`;
+    nav_item_txt.style.color = `black`;
+
+    nav_order_txt.style.fontSize = `20px`;
+    nav_order_txt.style.color = `black`;
+
+    nav_home_txt.style.fontSize = `20px`;
+    nav_home_txt.style.color = `black`;
+
+    nav_order_detail_txt.style.fontSize = `20px`;
+    nav_order_detail_txt.style.color = `black`;
+
+
+    main_root.style.position = `relative`;
+    main_root.style.left = `-100vw`;
+    main_root.style.transitionDuration = `1s`;
+});
