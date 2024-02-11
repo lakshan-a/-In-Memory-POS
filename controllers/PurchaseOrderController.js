68ee98232626bcd0493c1edb95295856c5883389
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-11 11:36:42 +0530

    updated

---
+++ controllers/PurchaseOrderController.js
@@ -0,0 +1,32 @@
+/**
+ * Disable Buttons
+ * */
+$("#btnAddToCart").attr('disabled', true);
+$("#btnPurchase").attr('disabled', true);
+
+/**
+ * Generate New Order ID
+ * */
+function generateOrderID() {
+    if (orders.length > 0) {
+        let lastId = orders[orders.length - 1].oId;
+        let digit = lastId.substring(6);
+        let number = parseInt(digit) + 1;
+        return lastId.replace(digit, number);
+    } else {
+        return "ODI-001";
+    }
+}
+
+/**
+ * Add Order Date
+ * */
+function setCurrentDate() {
+    let orderDate = $("#orderDate");
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
+    orderDate.val(today);
+}
