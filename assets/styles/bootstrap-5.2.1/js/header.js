4aaf853cfd1dae264f11a1d2a9dfbbe2c5f0134b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-11 20:33:05 +0530

    updated

--- assets/styles/bootstrap-5.2.1/js/header.js
+++ assets/styles/bootstrap-5.2.1/js/header.js
@@ -10,3 +10,26 @@ let nav_item_txt = document.getElementById("nav-item-txt");
 let nav_order_txt = document.getElementById("nav-order-txt");
 let nav_home_txt = document.getElementById("nav-home-txt");
 let nav_order_detail_txt = document.getElementById("nav-orderD-txt");
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
+}
+
+startTime();
