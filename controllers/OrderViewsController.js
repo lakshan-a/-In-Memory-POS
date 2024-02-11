215340e5881fdbb09c048ba28172cdff8ad35365
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-11 12:01:29 +0530

    updated

--- controllers/OrderViewsController.js
+++ controllers/OrderViewsController.js
@@ -8,3 +8,14 @@ function loadAllOrders() {
         $("#tblOrder").append(row);
     }
 }
+
+function loadAllOrderDetails() {
+
+    $("#tblOrderDetails").empty();
+
+    for (var orderDetail of orderDetails) {
+        console.log(orderDetail);
+        var row = `<tr><td>${orderDetail.orderId}</td><td>${orderDetail.cusId}</td><td>${orderDetail.itemId}</td><td>${orderDetail.qty}</td><td>${orderDetail.total}</td></tr>`;
+        $("#tblOrderDetails").append(row);
+    }
+}
