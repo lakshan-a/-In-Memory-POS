a4a8fa39cdf4f3a606301e96e174264b46a13e8e
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-11 20:44:28 +0530

    updated

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -42,15 +42,15 @@ $(document).ready(function () {
     });
 });
 
-// function setStatus(){
-//     $('#totOfCustomers').text(customers.length);
-//     $('#totOfItems').text(items.length);
-//     $('#totOfOrders').text(orders.length);
-//     $('#finishOrders').text(orders.length);
-//
-//
-//
-//     $('#income').text('55000');
-// }
-//
-// setStatus();
+function setStatus(){
+    $('#totOfCustomers').text(customers.length);
+    $('#totOfItems').text(items.length);
+    $('#totOfOrders').text(orders.length);
+    $('#finishOrders').text(orders.length);
+
+
+
+    $('#income').text('55000');
+}
+
+setStatus();
