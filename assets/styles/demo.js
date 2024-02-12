3fa4242531c6be93357da91c185c766744ac6078
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-12 19:45:14 +0530

    updated

--- assets/styles/demo.js
+++ assets/styles/demo.js
@@ -481,38 +481,5 @@ demo = {
             myChartData.update();
         });
 
-
-        // var ctx = document.getElementById("CountryChart").getContext("2d");
-        //
-        // var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
-        //
-        // gradientStroke.addColorStop(1, 'rgba(29,140,248,0.2)');
-        // gradientStroke.addColorStop(0.4, 'rgba(29,140,248,0.0)');
-        // gradientStroke.addColorStop(0, 'rgba(29,140,248,0)'); //blue colors
-
-
-        // var myChart = new Chart(ctx, {
-        //   type: 'bar',
-        //   responsive: true,
-        //   legend: {
-        //     display: false
-        //   },
-        //   data: {
-        //     labels: ['USA', 'GER', 'AUS', 'UK', 'RO', 'BR'],
-        //     datasets: [{
-        //       label: "Countries",
-        //       fill: true,
-        //       backgroundColor: gradientStroke,
-        //       hoverBackgroundColor: gradientStroke,
-        //       borderColor: '#1f8ef1',
-        //       borderWidth: 2,
-        //       borderDash: [],
-        //       borderDashOffset: 0.0,
-        //       data: [53, 20, 10, 80, 100, 45],
-        //     }]
-        //   },
-        //   options: gradientBarChartConfiguration
-        // });
-
     },
 };
