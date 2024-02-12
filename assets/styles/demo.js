dc9e917dd1596433590356d194d60ad5f7d89e7b
Lakshan Rashmika <124891151+lakshan-a@users.noreply.github.com>
2024-02-12 19:45:54 +0530

    updated

--- assets/styles/demo.js
+++ assets/styles/demo.js
@@ -108,54 +108,6 @@ demo = {
 
     initDashboardPageCharts: function() {
 
-        // gradientChartOptionsConfigurationWithTooltipBlue = {
-        //   maintainAspectRatio: false,
-        //   legend: {
-        //     display: false
-        //   },
-        //
-        //   tooltips: {
-        //     backgroundColor: '#f5f5f5',
-        //     titleFontColor: '#333',
-        //     bodyFontColor: '#666',
-        //     bodySpacing: 4,
-        //     xPadding: 12,
-        //     mode: "nearest",
-        //     intersect: 0,
-        //     position: "nearest"
-        //   },
-        //   responsive: true,
-        //   scales: {
-        //     yAxes: [{
-        //       barPercentage: 1.6,
-        //       gridLines: {
-        //         drawBorder: false,
-        //         color: 'rgba(29,140,248,0.0)',
-        //         zeroLineColor: "transparent",
-        //       },
-        //       ticks: {
-        //         suggestedMin: 60,
-        //         suggestedMax: 125,
-        //         padding: 20,
-        //         fontColor: "#2380f7"
-        //       }
-        //     }],
-        //
-        //     xAxes: [{
-        //       barPercentage: 1.6,
-        //       gridLines: {
-        //         drawBorder: false,
-        //         color: 'rgba(29,140,248,0.1)',
-        //         zeroLineColor: "transparent",
-        //       },
-        //       ticks: {
-        //         padding: 20,
-        //         fontColor: "#2380f7"
-        //       }
-        //     }]
-        //   }
-        // };
-
         gradientChartOptionsConfigurationWithTooltipPurple = {
             maintainAspectRatio: false,
             legend: {
